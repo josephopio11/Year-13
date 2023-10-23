9432d290b10a43a1c8d5835c9599d9a9cf0fc0eb
Joseph <josephopio11@gmail.com>
2023-10-23 11:58:00 +0800

    Added a comment

--- toughhomework.py
+++ toughhomework.py
@@ -4,6 +4,8 @@ something_crazy = sorts.insertion_sort
 
 list_fruits = ["mango", "apple", "banana", "abble"]
 
+# just a funny comment
+
 print(list_fruits)
 something_crazy(list_fruits)
 print(list_fruits)
